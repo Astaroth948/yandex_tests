7ef5531e2fceeb001e421ce327818589c9fe5b1d
Marat Salkaev <sw948@yandex.ru>
2021-08-10 00:32:20 +0300

    '10082021'

--- pages/locators.py
+++ pages/locators.py
@@ -4,4 +4,3 @@ from selenium.webdriver.common.by import By
 class YandexMainLocators():
     STATIC_NEWS_STRINGS = (By.XPATH, '//ol[@class="list news__list"]//a')
     DINAMIC_NEWS_STRING = (By.XPATH, '//ol[@class="list news__list news__animation-list"]/li[contains(@class, "list__item_fade_in")]/a')
-
